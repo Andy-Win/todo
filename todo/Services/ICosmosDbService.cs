4b4b1595c76319b6c8e649672d0035bbe4342519
Andy Wang <v-wajian@microsoft.com>
2021-03-29 01:41:32 -0700

    Add project files.

---
+++ todo/Services/ICosmosDbService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using todo.Models;
+namespace todo.Services
+{
+    public interface ICosmosDbService
+    {
+        Task<IEnumerable<Item>> GetItemsAsync(string query);
+        Task<Item> GetItemAsync(string id);
+        Task AddItemAsync(Item item);
+        Task UpdateItemAsync(string id, Item item);
+        Task DeleteItemAsync(string id);
+    }
+}
