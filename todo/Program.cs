fa5af3a13eba63230efbf8ce213e564e9cdc4afc
Andy Wang <v-wajian@microsoft.com>
2024-03-13 16:04:59 +0800

    Add project files.

---
+++ todo/Program.cs
@@ -0,0 +1,42 @@
+using todo;
+
+var builder = WebApplication.CreateBuilder(args);
+var section = builder.Configuration.GetSection("CosmosDb");
+builder.Services.AddSingleton<ICosmosDbService>(InitializeCosmosClientInstanceAsync(section).GetAwaiter().GetResult());
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Item}/{action=Index}/{id?}");
+
+app.Run();
+static async Task<CosmosDbService> InitializeCosmosClientInstanceAsync(IConfigurationSection configurationSection)
+{
+    var databaseName = "Tasks";
+    var containerName = "Item";
+    var account = configurationSection["AccountEndpoint"];
+    var key = configurationSection["AccountKey"];
+    var client = new Microsoft.Azure.Cosmos.CosmosClient(account, key);
+    var database = await client.CreateDatabaseIfNotExistsAsync(databaseName);
+    await database.Database.CreateContainerIfNotExistsAsync(containerName, "/id");
+    var cosmosDbService = new CosmosDbService(client, databaseName, containerName);
+    return cosmosDbService;
+}
